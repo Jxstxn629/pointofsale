565bf330e3e869900b7d779a055fefb6743e0888
Justin Ng <justin@Justins-MacBook-Air.local>
2024-08-27 15:06:27 +0800

    init

---
+++ src/main/java/com/example/pointofsale/PointofsaleApplication.java
@@ -0,0 +1,13 @@
+package com.example.pointofsale;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PointofsaleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PointofsaleApplication.class, args);
+	}
+
+}
